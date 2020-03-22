7d6df05681b2c31d485de5c42fa1748165b0854b
Nastya <nastya_mi@icloud.com>
2020-03-22 21:23:05 +0300

    feat: brackets is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (let i = 0; i < bracketsConfig.length; i++) {
+    const findBrackets = bracketsConfig[i].join('');
+
+    if (str.includes(findBrackets)) {
+        str = str.replace(findBrackets, '');
+        i = -1;
+    }
+}
+
+    return str.length === 0 ? true : false;
 }
